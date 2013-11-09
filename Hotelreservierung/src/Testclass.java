e9a95dc3ca154598fe6ff52c7c104d398e1b91d8
veronikalomasow <veronika.lomasow@gmx.at>
2013-11-09 13:42:03 +0100

    Update Testclass.java
    Erster Testversuch

--- Hotelreservierung/src/Testclass.java
+++ Hotelreservierung/src/Testclass.java
@@ -2,8 +2,8 @@ public class Testclass {
 
 	public static void main(String[] args) {
 		System.out.println("Testclass");
-		System.out.println("weisd warum sich die waldtraud nich in wald rein traut ? weils die edeltraud midm bredl haud");
 		System.out.println("commit vom Laptop");
+		System.out.println("Veronika war hier!");
 	}
 
 }
