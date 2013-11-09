06f2513023bdd799de2b7f16319a1523ad000a01
Korbson <korbson@gmx.net>
2013-11-09 13:40:51 +0100

    Update Testclass.java

--- Hotelreservierung/src/Testclass.java
+++ Hotelreservierung/src/Testclass.java
@@ -1,8 +1,8 @@
-
 public class Testclass {
 
 	public static void main(String[] args) {
 		System.out.println("Testclass");
+		System.out.println("weisd warum sich die waldtraud nich in wald rein traut ? weils die edeltraud midm bredl haud");
 		System.out.println("commit vom Laptop");
 	}
 
