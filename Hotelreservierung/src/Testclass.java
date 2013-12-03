98a8f194febbcd0dffc1ee3447a3d94d66136df4
MichiLazarus <michaellazarus@hotmail.de>
2013-12-03 19:14:39 +0100

    Testing.

--- Hotelreservierung/src/Testclass.java
+++ Hotelreservierung/src/Testclass.java
@@ -3,7 +3,6 @@ public class Testclass {
 	public static void main(String[] args) {
 		System.out.println("Testclass");
 		System.out.println("commit vom Laptop");
-		System.out.println("Veronika war hier!");
 	}
 
 }
